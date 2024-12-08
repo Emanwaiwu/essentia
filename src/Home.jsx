5ad91aad5fed473f597f2ea92f13ff9e3b17ef52
Emmanuel Nwaiwu <28emm@MacBookPro.lan>
2024-12-07 19:00:49 -0800

    text

--- src/Home.jsx
+++ src/Home.jsx
@@ -411,7 +411,7 @@ const Home = () => {
 
               <div className="flex justify-center">
                 {" "}
-                <div className="text-white text-center max-h-[1440px]">
+                <div className="font-semibold text-white text-center max-h-[1440px]">
                   {" "}
                   <motion.div
                     variants={buildVariant2}
@@ -480,7 +480,7 @@ const Home = () => {
 
           <section className=" p-7 max-w-[1440px] mx-auto" id="buildings">
             <div>
-              <h1 className="text-[16.5rem] text-center text-[#E7E7E7] md:text-[8.5rem] lg:text-[11.5rem] xl:text-[13.5rem] sm:text-[15vw]">
+              <h1 className="font-semibold text-[16.5rem] text-center text-[#E7E7E7] md:text-[8.5rem] lg:text-[11.5rem] xl:text-[13.5rem] sm:text-[15vw]">
                 PROJECTS
               </h1>
               <div className="heading-left">
@@ -588,7 +588,7 @@ const Home = () => {
           <section className=" overflow-visible" id="careers">
             <div className="p-7 max-w-[1440px] mx-auto sticky z-10">
               <div>
-                <h1 className="text-[16.5rem] leading-[95%] text-[#E7E7E7] md:text-[8.5rem] lg:text-[11.5rem] xl:text-[13.5rem] sm:text-[20vw]">
+                <h1 className="font-semibold text-[16.5rem] leading-[95%] text-[#E7E7E7] md:text-[8.5rem] lg:text-[11.5rem] xl:text-[13.5rem] sm:text-[20vw]">
                   Be Part of Our Crew
                 </h1>
                 <div className="heading">
@@ -851,7 +851,7 @@ const Home = () => {
                 </div>
               </div>
             </div>
-            <div className=" text-[#B3B3B3] text-center sm:mt-20 font-bold cut-off-text max-h-[1440px]">
+            <div className=" text-[#B3B3B3] text-center sm:mt-20 font-semibold cut-off-text max-h-[1440px]">
               <motion.div
                 variants={buildVariant}
                 initial="hidden"
